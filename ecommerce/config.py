6f62657be776e67ffb9b60a71be19a1282691e7d
Nil-Andreu <nilandreug@gmail.com>
2023-03-30 17:34:45 +0200

    [NEW] Database information

--- ecommerce/config.py
+++ ecommerce/config.py
@@ -1,5 +1,5 @@
 import os
-from pydantic import BaseSettings
+from pydantic import BaseSettings, PostgresDsn
 
 
 class Settings(BaseSettings):
@@ -7,7 +7,16 @@ class Settings(BaseSettings):
     APP_DESCRIPTION: str = 'Ecommerce developed with FastAPI'
     APP_VERSION: float = 1.0
 
-    DOCS_URL = os.getenv('DOCS_URL', '/docs')
+    DOCS_URL: str = os.getenv('DOCS_URL', '/docs')
+
+    # For database configuration
+    DATABASE_USERNAME: str = os.getenv('DATABASE_USERNAME')
+    DATABASE_PASSWORD: str = os.getenv('DATABASE_PASSWORD')
+    DATABASE_NAME: str = os.getenv('DATABASE_NAME')
+    DATABASE_HOST: str = os.getenv('DATABASE_HOST')
+    DATABASE_PORT: str = os.getenv('DATABASE_PORT', 5432)
+
+    DATABASE_URI: PostgresDsn = f'postgresq.://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}:{DATABASE_PORT}'
 
 
 def get_settings():
